75433c887a27049a913d27c2d32b2ec51e1ddb8d
oberoi1984 <41862558+oberoi1984@users.noreply.github.com>
2024-10-20 15:28:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "ap-south-1"  # Change to your preferred region
 }
+
 # Create a security group to allow SSH and HTTP access
 resource "aws_security_group" "ELK_sg" {
   name_prefix = "ELK-sg-"
@@ -24,18 +25,20 @@ resource "aws_security_group" "ELK_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "ELK-SecurityGroup"
   }
 }
-  
+
+# Create an EC2 instance for the ELK stack
 resource "aws_instance" "elk_server" {
   ami           = "ami-078264b8ba71bc45e"  # Use the latest Amazon Linux AMI or Ubuntu
   instance_type = "t2.micro"
   key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.ELK_sg.id]
 
-
-  
   user_data = <<-EOF
               #!/bin/bash
               # Update and install dependencies
